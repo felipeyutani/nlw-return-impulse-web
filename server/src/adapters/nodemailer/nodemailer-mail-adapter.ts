2b6bedbe8a76825830352f4fa6eb08f9ee4f3cd7
felipeyutani <lipe-1111@hotmailcom>
2022-06-28 04:36:25 -0300

    Add server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer'
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8473ec3bbfe07b",
+    pass: "7f297cf0ef3f55"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Felipe Yutani <felipeyutani@gmail.com>',
+      subject,
+      html: body,
+    })
+  }
+}
---
+++ server/src/routes.ts
@@ -0,0 +1,40 @@
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+import express from 'express';
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
+
+export const routes = express.Router()
+
+routes.post('/feedbacks', async (req, res) => {
+
+  const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const nodemailerMailAdapter = new NodemailerMailAdapter()
+
+
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  )
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  })
+
+  // await transport.sendMail({
+  //   from: 'Equipe Feedget <oi@feedget.com>',
+  //   to: 'Felipe Yutani <felipeyutani@gmail.com>',
+  //   subject: 'Novo feedback',
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `</div>`
+  //   ].join('\n')
+  // })
+
+  return res.status(201).send();
+})
